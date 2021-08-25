d84b3e5d247f60e59618538f5d85549938344d09
Vadym Stebakov <vadim.stebakov.94@gmail.com>
2021-08-25 17:30:38 +0300

    finish app

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,42 @@
-import {useCallback, useEffect, useState} from 'react'
-import axios from 'axios'
-import {List} from './List'
-import {Form} from './Form'
+import { useCallback, useEffect, useState } from 'react';
+import axios from 'axios';
+import { List } from './List';
+import { Form } from './Form';
+
+const baseURL = process.env.NODE_ENV === 'production' ? 'http://localhost:5000' : 'http://localhost:5000';
+
+const api = axios.create({
+    baseURL,
+});
 
 function App() {
-  const [notes, setNotes] = useState([])
+    const [notes, setNotes] = useState([]);
 
-  async function createNote(text) {
-    const note = await axios.post('/api/note', {text})
-    setNotes([...notes, {...note.data.note}])
-  }
+    async function createNote(text) {
+        const note = await api.post('/api/note', { text });
+        setNotes([...notes, { ...note.data.note }]);
+    }
 
-  const fetchNotes = useCallback(async () => {
-    const notes = await axios.get('/api/note')
-    setNotes(notes.data)
-  }, [])
+    const fetchNotes = useCallback(async () => {
+        const notes = await api.get('/api/note');
+        setNotes(notes.data);
+    }, []);
 
-  useEffect(() => {
-    fetchNotes()
-  }, [fetchNotes])
+    useEffect(() => {
+        fetchNotes();
+    }, [fetchNotes]);
 
-  return (
-    <>
-      <nav className="navbar">
-        <h3>Docker MERN</h3>
-      </nav>
-      <div className="container with-nav">
-        <Form onCreate={createNote} />
-        <List list={notes} />
-      </div>
-    </>
-  )
+    return (
+        <>
+            <nav className="navbar">
+                <h3>Docker MERN</h3>
+            </nav>
+            <div className="container with-nav">
+                <Form onCreate={createNote} />
+                <List list={notes} />
+            </div>
+        </>
+    );
 }
 
 export default App;
--- server/routes/note.routes.js
+++ server/routes/note.routes.js
@@ -1,19 +1,19 @@
-const {Router} = require('express')
-const Note = require('../models/note')
+const { Router } = require('express');
+const Note = require('../models/note');
 
-const router = Router()
+const router = Router();
 
 router.post('/', async (req, res) => {
-  const text = req.body.text
-  const note = new Note({ text })
-  await note.save()
+    const text = req.body.text;
+    const note = new Note({ text });
+    await note.save();
 
-  res.status(201).json({ note })
-})
+    res.status(201).json({ note });
+});
 
 router.get('/', async (req, res) => {
-  const notes = await Note.find()
-  res.json(notes)
-})
+    const notes = await Note.find();
+    res.json(notes);
+});
 
-module.exports = router
+module.exports = router;
